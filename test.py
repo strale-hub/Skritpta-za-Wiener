e5738ba2c137c4b034252e864694f5722882d91c
Jasna Miletić <jasna.jovic@coming.local>
2020-05-06 16:36:51 +0200

    Sredjeni argumenti

--- test.py
+++ test.py
@@ -1,20 +1,31 @@
 import pandas
 import argparse
 
+# Definisanje potrebnih argumenata i ucitavanje csv fajla
+
 parser = argparse.ArgumentParser(description = 'Argumenti')
-parser.add_argument ("-u", help = "This is a filtered user")
+parser.add_argument ("-u", "-user", nargs="+", help = "Filter output by one or more space separated users", metavar='')
+parser.add_argument ("-l", "-last", action='store_true', help = "Display last log for each user")
+
+args = parser.parse_args()
 
 
 df = pandas.read_csv("oldPDF.csv", 
 	header = 0,
 	names = ["Time", "SourceUser", "VPNGroup"])
 
-#user = input("unesi usera:\n")
+# Filtriranje po userima
 
-args = parser.parse_args()
-user = args.u
+if args.u:
+	df = (df[df.SourceUser.isin(args.u)])
+	print(df.reset_index(drop=True))
 
-print(df[df.SourceUser.eq(user)])
+# Filtriranje poslednjeg logovanja
 
-#df = df.drop_duplicates(subset=['SourceUser'], keep = 'last')
-#print(df.reset_index(drop=True))
+elif args.l:
+	df = df.drop_duplicates(subset=['SourceUser'], keep = 'last')
+	print(df.reset_index(drop=True))
+
+else: 
+	print("Potrebno je definisati parametar -u ili -l.\n\nZa vise informacija, pogledajte --help")
+
